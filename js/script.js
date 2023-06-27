cb579c584685b6be658939d0f2b2d5d62729b8a0
Abhilasha Agrawal <abhilashaagrawal27@gmail.com>
2023-06-27 18:01:28 +0530

    playing game

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+
+let saveRegInfo=()=>{
+    let fn=document.getElementById('First_name').value;
+    let ln=document.getElementById('Last_name').value;
+    console.log(fn);
+    console.log(ln);
+    window.localStorage.setItem('First_name',fn);
+    window.localStorage.setItem('Last_name',ln);
+    window.location.reload();
+}
+let logOut=()=>{
+    //console.log('okkk');
+    window.localStorage.clear();
+    window.location.reload();
+}
+//()();
+(()=>{
+    let fn=window.localStorage.getItem('First_name');
+    let ln=window.localStorage.getItem('Last_name');
+
+    //console.log('Page loaded Successfully');
+    
+    // Get a reference to the modal element
+    var modal=document.getElementById('registrationModal');
+
+    // create a bootstrap modal instance using the modal element
+    var modalInstance= new bootstrap.Modal(modal);
+    if((fn===null)&&(ln===null)){
+        //call the `show` method on the modal instance to launch the modal
+        modalInstance.show();
+        
+    }
+    if(fn!==null){
+        document.querySelector('.a_welcome').innerHTML='Welcome '+fn +" "+ln +'<button class="btn btn-sm ms-3 bg-danger text-white" onclick="logOut()">Log Out</button>';
+    }
+})();
